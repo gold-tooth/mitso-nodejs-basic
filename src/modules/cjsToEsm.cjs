1d5580470dd9a841424003b06938438d913e002b
danik stavskiy <danik.stavskiy@gmail.com>
2023-03-12 18:03:10 +0300

    add modules

--- src/modules/cjsToEsm.cjs
+++ src/modules/cjsToEsm.cjs
@@ -1,16 +1,19 @@
-const path = require('path');
-const { release, version } = require('os');
-const { createServer: createServerHttp } = require('http');
-require('./files/c');
+import path from 'path';
+import { createServer as createServerHttp} from 'http';
+import { fileURLToPath  } from 'url';
+import { release, version } from 'os';
+import './files/c.js';
+import data1 from './files/a.json' assert { type: "json" };
+import data2 from './files/b.json' assert { type: "json" };
 
 const random = Math.random();
 
 let unknownObject;
 
 if (random > 0.5) {
-    unknownObject = require('./files/a.json');
+    unknownObject = data1;
 } else {
-    unknownObject = require('./files/b.json');
+    unknownObject = data2;
 }
 
 console.log(`Release ${release()}`);
@@ -33,8 +36,8 @@ myServer.listen(PORT, () => {
     console.log('To terminate it, use Ctrl+C combination');
 });
 
-module.exports = {
+export {
     unknownObject,
-    myServer,
-};
+    myServer
+}
 
