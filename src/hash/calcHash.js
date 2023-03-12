4e608704ba14d719d3121eae58d41dedf01f6037
danik stavskiy <danik.stavskiy@gmail.com>
2023-03-12 17:41:19 +0300

    add hash

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,15 @@
-const calculateHash = async () => {
-    // Write your code here 
+import fs from "fs/promises";
+import url from "url";
+import {createHash, createHmac} from "crypto";
+import path from "path";
+
+const __dirname = url.fileURLToPath(new URL(".", import.meta.url))
+const filepath = path.join(__dirname, "files", "fileToCalculateHashFor.txt");
+
+
+const calculateHash = async () => { 
+    let date = await fs.readFile(filepath);
+    console.log(createHmac('sha256', date).update(date).digest('hex'));
 };
 
 await calculateHash();
