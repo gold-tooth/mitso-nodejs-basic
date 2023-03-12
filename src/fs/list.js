7745f2de916b1ea476b0ed68d3cbc02468be0e60
danik stavskiy <danik.stavskiy@gmail.com>
2023-03-12 17:10:48 +0300

    add list.js

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,22 @@
+import fs from "fs/promises";
+import * as url from "url";
+let __dirname = url.fileURLToPath(new URL(".", import.meta.url));
+
+
 const list = async () => {
-    // Write your code here 
+    let file = await fs.readdir(`${__dirname}files`, (err) =>
+    {
+        if (err)
+        {
+            throw new error ("FS operation failed");
+        }
+        else
+        {
+            console.log("Ура данные будут!!!!");
+        }
+    }); 
+
+    console.log(file);
 };
 
 await list();
